e6408dd1a1bf6e7a2fb6e677a8f2c38803491fc8
Zhu-jiatong <64255832+Zhu-jiatong@users.noreply.github.com>
2022-02-24 21:04:13 +0800

    first commit

---
+++ ComixReader.bat
@@ -0,0 +1,33 @@
+@echo off
+setlocal enabledelayedexpansion
+set /a k+=1
+echo ^<body^>>Comix.html
+set /a k+=1
+echo ^<div style="text-align: center; width: 100%%% %; height: 100%%% %; display: inline-block;"^>>>Comix.html
+for %%a in (*.webp) do (
+    set /a i+=1
+    set /a j=i+1
+    set /a k+=1
+    echo ^<a id="!i!"^>>>Comix.html
+    set /a k+=1
+    echo ^<a href="#!j!"^>>>Comix.html
+    set /a k+=1
+    echo ^<img src="%%a" style="max-height: 100%%% %; max-width: 100%%% %;"^>>>Comix.html
+    set /a k+=1
+    echo ^</a^>>>Comix.html
+    set /a k+=1
+    echo ^</a^>>>Comix.html
+)
+set /a k=k-3
+for /f "tokens=1 delims=    " %%a in (Comix.html) do (
+    set /a l+=1
+    if !l!==!k! (
+        echo ^<a href="#1"^>>>temp.txt
+    ) else echo %%a>>temp.txt
+)
+del Comix.html
+ren temp.txt Comix.html
+echo ^</div^>>>Comix.html
+echo ^</body^>>>Comix.html
+start Comix.html
+exit
